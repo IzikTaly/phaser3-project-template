26f9962724a93cf65304a58b9b4dfb460d7d21e8
Isaac Talley <izik.taly@gmail.com>
2019-06-13 15:44:39 -0600

    all done

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,20 @@
+import 'phaser'
+
+export default class GameScene extends Phaser.scene {
+    constructor() {
+        super('Game');
+
+    }  
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+}
+
+
+  
